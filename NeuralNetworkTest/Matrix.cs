eead8e9a24bf28fb1202a3e9c935999d02331fd5
theguru817 <nathanpohle@gmail.com>
2018-02-21 17:50:15 +1000

    Initial Commit

---
+++ NeuralNetworkTest/Matrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworkTest.MatrixHelper
+{
+    public class Matrix
+    {
+        public List<MatrixRow> matrixData;
+
+        public Matrix(double[][] marray)
+        {
+            matrixData = new List<MatrixRow>();
+            for (int i=0; i<marray.Length; i++ )
+            {
+                MatrixRow mRow = new MatrixRow(marray[i]);
+                matrixData.Add(mRow);
+            }
+        }
+
+        public Matrix()
+        {
+            matrixData = new List<MatrixRow>();
+
+        }
+
+    }
+
+    public class MatrixRow
+    {
+        internal List<MatrixCol> matrixColData;
+
+        internal MatrixRow(double[] mColData)
+        {
+            matrixColData = new List<MatrixCol>();
+            foreach (double item in mColData)
+            {
+                MatrixCol matrixCol = new MatrixCol(item);
+                matrixColData.Add(matrixCol);
+            }
+
+        }
+
+
+    }
+
+    internal class MatrixCol
+    {
+        internal double mdata;
+
+        internal MatrixCol(double mdata)
+        {
+            this.mdata = mdata;
+        }
+    }
+}
---
+++ NeuralNetworkTest/MatrixMath.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworkTest.MatrixHelper
+{
+    public class MatrixMath
+    {
+        public Matrix matrix;
+
+        public MatrixMath (Matrix matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        public void ScalarMultiply (double scalarFactor)
+        {
+            foreach (MatrixRow rows in matrix.matrixData)
+            {
+                foreach (MatrixCol colValues in rows.matrixColData)
+                {
+                    colValues.mdata *= scalarFactor;
+                }
+            }
+        }
+
+        public void DotMultiply (Matrix secondMatrix)
+        {
+            Matrix resultMatrix = new Matrix();
+            int mFirstColCount = matrix.matrixData[0].matrixColData.Count;
+            int mSecondRowCount = secondMatrix.matrixData.Count;
+            
+            if (mFirstColCount==mSecondRowCount)
+            {
+                foreach (MatrixRow mRow in matrix.matrixData)
+                {
+                    foreach (MatrixCol mCol in mRow.matrixColData)
+                    {
+                        double newColumn = mCol.mdata
+                    }
+                }
+            }
+        }
+        
+
+    }
+
+    
+}
