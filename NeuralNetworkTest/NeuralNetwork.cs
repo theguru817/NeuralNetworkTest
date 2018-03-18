62588abbccd25eea38b31b63bfc5e81cf7461a84
Nathan Pohle <nathanpohle@gmail.com>
2018-03-18 16:22:44 +1000

    Added sigmoid function to feedforward method and finished off backpropogation method. Tested a training and prediction session.

--- NeuralNetworkTest/NeuralNetwork.cs
+++ NeuralNetworkTest/NeuralNetwork.cs
@@ -12,6 +12,9 @@ namespace NeuralNetworkTest
         private double[,] inputsArray;
         private int numHiddenNodes;
         private int numOutputNodes;
+        private double learningRate=0.2;
+        private MatrixHelper.Matrix outputBias;
+        private MatrixHelper.Matrix hiddenBias;
 
         public NeuralNetwork(double[,] inputsArray, int numHiddenNodes, int numOutputNodes)
         {
@@ -57,28 +60,51 @@ namespace NeuralNetworkTest
             {
                 hiddenBiasArray[i, 0] = (new Random()).NextDouble();
             }
-
-            MatrixHelper.Matrix hiddenBias = new MatrixHelper.Matrix(hiddenBiasArray);
+            hiddenBias = new MatrixHelper.Matrix(hiddenBiasArray);
 
             double[,] outputBiasArray = new double[numOutputNodes, 1];
             for (int i = 0; i < numOutputNodes; i++)
             {
                 outputBiasArray[i, 0] = (new Random()).NextDouble();
             }
-
-            MatrixHelper.Matrix outputBias = new MatrixHelper.Matrix(outputBiasArray);
+            outputBias = new MatrixHelper.Matrix(outputBiasArray);
 
             hiddenNodes = inputWeightsMatrix.DotMultiply(inputNodes).Add(hiddenBias);
+
+            foreach (MatrixHelper.MatrixRow mRow in hiddenNodes)
+            {
+                foreach (MatrixHelper.MatrixCol mCol in mRow)
+                {
+                    mCol.data = 1 + Math.Pow(Math.E, (-1) * mCol.data);
+                }
+            }
+
             outputNodes = hiddenWeightsMatrix.DotMultiply(hiddenNodes).Add(outputBias);
 
+            foreach (MatrixHelper.MatrixRow mRow in outputNodes)
+            {
+                foreach (MatrixHelper.MatrixCol mCol in mRow)
+                {
+                    mCol.data = 1 + Math.Pow(Math.E, (-1) * mCol.data);
+                }
+            }
         }
 
         public void PerformBackPropogation(MatrixHelper.Matrix desiredOutput)
         { 
             MatrixHelper.Matrix outputNeg = outputNodes.ScalarMultiply(-1);
             MatrixHelper.Matrix outputError = desiredOutput.Add(outputNeg);
-            
+            MatrixHelper.Matrix hiddenError = hiddenWeightsMatrix.Transpose().DotMultiply(outputError);
+            MatrixHelper.Matrix deltaWeightsHO = outputError.HadamardProduct(outputNodes.Add(outputNodes.HadamardProduct(outputNodes).ScalarMultiply(-1))).ScalarMultiply(learningRate).DotMultiply(hiddenNodes.Transpose());
+            MatrixHelper.Matrix deltaWeightsIH = hiddenError.HadamardProduct(hiddenNodes.Add(hiddenNodes.HadamardProduct(hiddenNodes).ScalarMultiply(-1))).ScalarMultiply(learningRate).DotMultiply(inputNodes.Transpose());
+            MatrixHelper.Matrix deltaBiasO = outputError.HadamardProduct(outputNodes.Add(outputNodes.HadamardProduct(outputNodes).ScalarMultiply(-1))).ScalarMultiply(learningRate);
+            MatrixHelper.Matrix deltaBiasH = hiddenError.HadamardProduct(hiddenNodes.Add(hiddenNodes.HadamardProduct(hiddenNodes).ScalarMultiply(-1))).ScalarMultiply(learningRate);
 
+            hiddenWeightsMatrix = hiddenWeightsMatrix.Add(hiddenWeightsMatrix.Add(deltaWeightsHO));
+            inputWeightsMatrix = inputWeightsMatrix.Add(inputWeightsMatrix.Add(deltaWeightsIH));
+
+            outputBias = outputBias.Add(deltaBiasO);
+            hiddenBias = hiddenBias.Add(deltaBiasH);
         }
 
     }
--- NeuralNetworkTest/Program.cs
+++ NeuralNetworkTest/Program.cs
@@ -8,7 +8,12 @@ namespace NeuralNetworkTest
         private static void Main(string[] args)
         {
             double[,] inputArray = new double[,] { { 1 }, { 0 } };
+            double[,] outputArray = new double[,] { { 1 } };
+            Matrix desiredOutput = new Matrix(outputArray);
             NeuralNetwork nn = new NeuralNetwork(inputArray, 2, 1);
+
+            nn.PerformFeedForward();
+            nn.PerformBackPropogation(desiredOutput);
             nn.PerformFeedForward();
 
             Console.WriteLine("Done.");
